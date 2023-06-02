ff7aaa2f6f9fca4e299a7d39d025d086103dc58f
Ross Magparangalan <rossmagp@gmail.com>
2023-06-02 12:07:20 -0400

    set up basic directory and routing for client-side

---
+++ client/src/pages/HomePage/HomePage.js
@@ -0,0 +1,16 @@
+import "./HomePage.scss";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+
+function HomePage() {
+
+    return (
+        <div>
+            Bark Break
+        </div>
+    );
+}
+
+export default HomePage
+
