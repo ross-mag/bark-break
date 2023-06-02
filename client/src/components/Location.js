bf860fbd6d5f9c421560526a2ba5e333e4297ff1
Karimah Nichols <ms.karimah.nichols@gmail.com>
2023-06-02 14:38:18 -0400

    add lcoation page and styling

---
+++ client/src/components/Location.js
@@ -0,0 +1,23 @@
+import 'locations.scss';
+import Home from '../pages/HomePage';
+
+// we want to display a location card that references our database
+
+export default function Location(props){
+    const {locationObj} = props;
+  
+    return (
+        <div className="card">
+        <Link to= {`/location/${location.id}`} className="card_router-link">
+            <h3 className="card_location">{locationObj.location}</h3>
+            <h4 className="card_distance">{locationObj.distance}</h4>
+            <h4 className="card_eta">Time to get there: {locationObj.time}</h4>
+            <h4 className="card_category">Category: {locationObj.category}</h4>
+        </Link>
+        <div className='card_map-link'>
+            <p> {locationObj.google-maps-link}</p>
+        </div>
+        </div>
+)
+
+}
